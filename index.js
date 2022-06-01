fe70ec9a05c03073fcffa13ff9e17f71971fbfd3
ndukwesamuel <ndukwesamuel23@yahoo.com>
2022-06-01 16:17:01 +0100

    dfjfdkjf

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("<h1> Samheart</h1>");
+app.use(express.static(path.join(__dirname, "/my-app")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/my-app/build/", "index.html"));
 });
+
+// app.get("/", (req, res) => {
+//   res.send("<h1> Samheart</h1>");
+// });
 app.listen(port, () => {
   console.log("Backend server is running!");
 });
