570e2a47d3526e0ba79b4a80552b2de8b6bb60c3
ndukwesamuel <ndukwesamuel23@yahoo.com>
2022-06-02 02:57:54 +0100

    fdfdfdf

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
 
+const Blog = require("./models/PostModel");
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, "/my-app")));
+// app.use(express.static(path.join(__dirname, "/my-app")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/my-app/build/", "index.html"));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/my-app/build/", "index.html"));
+// });
+
+mongoose
+  .connect("mongodb://localhost:27017/the_new_text")
+  .then((result) => console.log("connect db"))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/testing_blog", (req, res) => {
+  // we want to first create the instance of the blog
+  const blog = new Blog({
+    title: "new blog 2",
+    snippet: "about my new blog",
+    body: "more about my new blog",
+  });
+
+  blog
+    .save()
+    .then((result) => {
+      // res.render("index");
+      res.json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-// app.get("/", (req, res) => {
-//   res.send("<h1> Samheart</h1>");
-// });
+app.get("/", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.listen(port, () => {
   console.log("Backend server is running!");
 });
