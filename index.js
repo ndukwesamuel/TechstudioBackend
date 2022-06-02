9fbd17d32dabc15292adfb29113982c0db731376
ndukwesamuel <ndukwesamuel23@yahoo.com>
2022-06-02 03:47:21 +0100

    dfdf

--- index.js
+++ index.js
@@ -44,18 +44,7 @@ app.get("/", (req, res) => {
     });
 });
 
-// app.use(express.static(path.join(__dirname, "/my-app")));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/my-app/build", "index.html"));
-// });
-
-app.use(express.static(path.join(__dirname, "/my-app/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/my-app/build", "index.html"));
-});
-
+// this is new
 app.listen(port, () => {
   console.log("Backend server is running!");
 });
